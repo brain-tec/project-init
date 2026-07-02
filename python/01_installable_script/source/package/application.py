2c56d048679c2ab638afe806a1acc97b5f066baa
Phil Gaiser <phil.gaiser@raven-computing.com>
2026-07-03 00:17:18 +0800

    Improved Python installable script project source template.
    Removed pointless argc parameter in main() function.
    
    Signed-off-by: Phil Gaiser <phil.gaiser@raven-computing.com>

--- python/01_installable_script/source/package/application.py
+++ python/01_installable_script/source/package/application.py
@@ -4,22 +4,21 @@ ${{VAR_COPYRIGHT_HEADER}}
 import sys
 
 
-def main(argc: int, argv: list[str]) -> int:
+def main(argv: list[str]) -> int:
     """Main function of the ${{VAR_PROJECT_NAME}} application.
 
     Args:
-        argc (int): The argument count.
-        argv (list[str]): The list of program arguments.
+        argv (list): The list of str program arguments.
 
     Returns:
         int: The exit code of the program.
     """
     print("${{VAR_PROJECT_SLOGAN_STRING}}")
-    if argc > 1:
+    if len(argv) > 1:
         print("Args: " + str(argv[1:]))
 
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main(len(sys.argv), sys.argv))
+    sys.exit(main(sys.argv))
